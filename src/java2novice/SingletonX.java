fb9ee0f4cad7106d54f2dad928d92cbb3c2cf501
Ladydiana <diana.culincu@gmail.com>
2018-02-02 18:00:57 +0200

    Write a singleton class.

---
+++ src/java2novice/SingletonX.java
@@ -0,0 +1,35 @@
+/*
+ * Write a singleton class
+ */
+
+package java2novice;
+
+
+// Singleton -> you can create only 1 object of the class
+public class SingletonX {
+    
+    private static SingletonX singlton;
+    
+    //initializing in a static block executed when the class is loaded
+    static {
+        singlton = new SingletonX();
+    }
+    
+    // making constructor private to prevent extra initializing
+    private SingletonX () {
+    }
+    
+    public static SingletonX getSingletonX(){
+        return singlton;
+    }
+    
+    public void test () {
+        System.out.println("Creating the singleton success.");
+    }
+    
+    public static void main (String args[]) {
+        SingletonX ms = SingletonX.getSingletonX();
+        ms.test();
+    }
+    
+}
