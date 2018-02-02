58c30d7abc00b8351c3fda8a0b3e4f5f44df3308
Ladydiana <diana.culincu@gmail.com>
2018-02-02 23:05:44 +0200

    Write a program to reverse a number.

--- src/java2novice/NumberReverse.java
+++ src/java2novice/NumberReverse.java
@@ -25,17 +25,26 @@ public class NumberReverse {
     
     // Solution 1 - We treat the number as String and use the previously written String Recursive class
     public void Solution1 () {
-        System.out.println ("Solution 1 reseult: " + 
+        System.out.println ("Solution 1 result: " + 
                             new StringReverseRecursive().reverseRecursive(Integer.toString(this.number)));
         
     }
     
-    
+    // Solution 2 - We do it the mathematical way
+    public void Solution2 () {
+        int reverse = 0;
+        while (this.number!=0) {
+            reverse = reverse*10 + this.number%10;
+            this.number = this.number/10;
+        }
+        System.out.println ("Solution 2 result: " + reverse);
+    }
     
     public static void main (String args[]) {
         NumberReverse nr = new NumberReverse();
         nr.readNumber();
         nr.Solution1();
+        nr.Solution2();
     }
 
 
