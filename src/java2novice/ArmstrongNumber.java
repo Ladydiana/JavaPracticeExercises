3b301aae8791c88912be6381929d32b8bd680d24
Ladydiana <diana.culincu@gmail.com>
2018-02-05 16:31:01 +0200

    Write a program to find if a given number is an Armstrong number.

---
+++ src/java2novice/ArmstrongNumber.java
@@ -0,0 +1,30 @@
+/*
+ * Write a program to find if a given number is an Armstrong number.
+ */
+
+package java2novice;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    
+    public boolean findIfArmstrong(int number) {
+        int sum = 0;
+        int numberOfDigits = String.valueOf(number).length();
+        int numberCopy = number;
+                
+        while(numberCopy>0) {
+            sum += Math.pow(numberCopy%10, numberOfDigits);
+            numberCopy /= 10;
+        }
+        
+        if(number == sum)
+            return true;
+        return false;
+    }
+    
+    public static void main(String args[]) {
+        System.out.println("Insert number to check:");
+        System.out.println(new ArmstrongNumber().findIfArmstrong(new Scanner(System.in).nextInt()));
+    }
+}
