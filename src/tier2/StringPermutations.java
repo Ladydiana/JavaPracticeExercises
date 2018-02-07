3f0b4fe9bec304735f2e2631568fa48cc30922ca
Ladydiana <diana.culincu@gmail.com>
2018-02-07 13:17:01 +0200

    Write a program to print all permutations of a given string.

--- src/tier2/StringPermutations.java
+++ src/tier2/StringPermutations.java
@@ -5,17 +5,27 @@
 package tier2;
 
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class StringPermutations {
     
-    public void permuteString(String s) {
+    public void permuteString(String first, String s, TreeSet<String> h) {
+        h.add(first + s);
         
+        for(int i=0; i<s.length(); i++) {
+            permuteString(  first + s.charAt(i), 
+                            s.substring(0, i) + s.substring(i+1), h);
+        }
     }
     
     public static void main(String args[]) {
         System.out.println("Insert string to permute: ");
         String s = new Scanner(System.in).nextLine();
+        TreeSet<String> permSet = new TreeSet<>();
         
-        new StringPermutations().permuteString(s);
+        System.out.println("\nPERMUTATIONS: ");
+        new StringPermutations().permuteString("",s, permSet);
+        
+        System.out.println(permSet);
     }
 }
