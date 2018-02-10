ae5af347b829b51cf2bdc44a480fe44cf3841b70
Ladydiana <diana.culincu@gmail.com>
2018-02-11 01:57:34 +0200

    Find the intersection point in the graphs of 2 given functions.

---
+++ src/Set2/FunctionIntersection.java
@@ -0,0 +1,29 @@
+/*
+ * We have functions f,g :R->R, where 
+ * f(x)=a*x+b and 
+ * g(x)=c*x+d
+ * Find the intersection point in the graphs of thse 2 functions.
+ * Since these are linear functions, their graphs are lines.
+ * We consider that this point has (x, y) coordinates. So, at the point of \
+ * intersection the (x, y) coordinates for function 1 equal the (x, y) \
+ * coordinates for function 2.
+ * Since at the point of intersection the two y-coordinates are equal, the \
+ * y-coordinate from Line 1 equal to the y-coordinate from Line 2.
+ * The y-coordinate for Line 1 is calculated this way:
+ * y = ax + b
+ * The y-coordinate for Line 2 is calculated this way:
+ * y = cx + d
+ * Setting the two y-coordinates equal looks like this:
+ * ax + b = cx + d
+ * From here we can find out the x using algebra.
+ * ax - cx = d - b
+ * x(a - c) = d - b
+ * x = (d - b)/(a - c)
+ */
+
+package Set2;
+
+
+public class FunctionIntersection {
+    
+}
