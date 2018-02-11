4739a2a61f0e00723625afe75c023498ef7b1c53
Ladydiana <diana.culincu@gmail.com>
2018-02-11 20:30:54 +0200

    You read a number of 3 digits. Eliminate 1 digit so that you obtain the biggest number you can formed by the remaining 2 digits in their respective order.

--- src/Set2/BiggestNumberByElimination.java
+++ src/Set2/BiggestNumberByElimination.java
@@ -10,6 +10,13 @@ import java.util.Scanner;
 public class BiggestNumberByElimination {
     int number;
     
+    public void checkNumber() {
+        if(number/100==0 || number/100>9) {
+            System.err.println("Number doesn't have 3 digits.");
+            System.exit(0);
+        }
+    }
+    
     public void readNumber() {
         System.out.print("number= ");
         Scanner s = new Scanner(System.in);
@@ -17,8 +24,24 @@ public class BiggestNumberByElimination {
         s.close();
     }
     
+    public void eliminateDigit() {
+        int max = Integer.MIN_VALUE;
+        
+        if(max < number/10)
+            max = number/10;
+        if(max < number%100)
+            max = number%100;
+        if(max < number/100 * 10 + number%10)
+            max=number/100 * 10 + number%10;
+        
+        System.out.println("Max: " + max);
+        
+    }
+    
     public static void main(String args[]) {
         BiggestNumberByElimination bnbe = new BiggestNumberByElimination();
         bnbe.readNumber();
+        bnbe.checkNumber();
+        bnbe.eliminateDigit();
     }
 }
