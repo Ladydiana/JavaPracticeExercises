798afa4a05019c464932973359e6039fad399966
Ladydiana <diana.culincu@gmail.com>
2018-02-11 20:32:05 +0200

    You read a number of 3 digits. Eliminate 1 digit so that you obtain the biggest number you can formed by the remaining 2 digits in their respective order.

--- src/Set2/BiggestNumberByElimination.java
+++ src/Set2/BiggestNumberByElimination.java
@@ -1,6 +1,8 @@
 /*
  * You read a number of 3 digits. 
- * Eliminate 1 digit so that you obtain the biggest number you can formed by the remaining 2 digits in their respective order.  
+ * Eliminate 1 digit so that you obtain the biggest number you can formed by \
+ * the remaining 2 digits in their respective order.
+ * For example: 796 -> we eliminate 7 and get 96
  */
 
 package Set2;
