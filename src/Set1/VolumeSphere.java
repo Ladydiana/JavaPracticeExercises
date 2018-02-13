cac4ec293dc213be46c8ee6faab29e1a7298e6c5
Ladydiana <diana.culincu@gmail.com>
2018-02-13 17:25:32 +0200

    Find the volume of a sphere of radius r.

---
+++ src/Set1/VolumeSphere.java
@@ -0,0 +1,38 @@
+/*
+ *  Find the volume of a circle of radius r.
+ */
+
+package Set1;
+
+import java.util.Scanner;
+
+public class VolumeSphere {
+    
+    double r;
+    
+    public void readRadius() {
+        System.out.print("r= ");
+        Scanner s = new Scanner(System.in);
+        this.r = s.nextDouble();
+        s.close();
+    }
+    
+    public void checkRadius() {
+        if(r<=0) {
+            System.err.println("Length has to be greater than 0.");
+            System.exit(0);
+        }
+    }
+    
+    public void computeVolume() {
+        System.out.println("Perimeter: " + Math.pow(r,3)*4/3+ "π = " + 
+                            (Math.pow(r,3)*4/3*Math.PI));
+    }
+    
+    public static void main(String args[]) {
+        VolumeSphere vs = new VolumeSphere();
+        vs.readRadius();
+        vs.checkRadius();
+        vs.computeVolume();
+    }
+}
