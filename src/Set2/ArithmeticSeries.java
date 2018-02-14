1dbdcb26b257ba89bb451c5aa5adb422d0c36bd8
Ladydiana <diana.culincu@gmail.com>
2018-02-14 16:39:35 +0200

    Find the sum of an arithmetic series, where the first element is a1, common difference d and n number of terms.

---
+++ src/Set2/ArithmeticSeries.java
@@ -0,0 +1,11 @@
+/*
+ * Find the sum of an arithmetic series, where the first element is a1, common difference d and n number of terms.
+ */
+
+package Set2;
+
+import java.util.Scanner;
+
+public class ArithmeticSeries {
+    
+}
