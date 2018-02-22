fc098a3c2f91583fc4ba1b553e68beb225a6ccd0
Ladydiana <diana.culincu@gmail.com>
2018-02-22 20:46:35 +0200

    Print the number of digits forming number n.

---
+++ src/Set2/DigitNumber.java
@@ -0,0 +1,48 @@
+/*
+ * Print the number of digits forming number n.
+ */
+
+package Set2;
+
+import java.util.Scanner;
+
+public class DigitNumber {
+    
+    int n, noOfDigits=0;
+
+    public void readNumber() {
+        System.out.print("n= ");
+        Scanner s = new Scanner(System.in);
+        this.n = s.nextInt();
+        s.close();
+    }
+    
+    public void checkNumber() {
+        if(n<0) {
+            n *= -1;
+        }
+    }
+    
+    // Solution 1, making use of Java conversion
+    public int numberOfDigitsUsingConversion() {
+        return Integer.toString(n).length();
+    }
+    
+    //Solution 2
+    public int numberOfDigitsUsingDivision(int n) {
+        if(n<=0)
+            return 0;
+        else
+            return 1 + numberOfDigitsUsingDivision(n/10);
+    }
+    
+    public static void main(String args[]) {
+        DigitNumber dn = new DigitNumber();
+        dn.readNumber();
+        dn.checkNumber();
+        System.out.println("Number of digits using string conversion: " + 
+                            dn.numberOfDigitsUsingConversion());
+        System.out.println("Number of digits using division: " + 
+                            dn.numberOfDigitsUsingDivision(dn.n));
+    }
+}
