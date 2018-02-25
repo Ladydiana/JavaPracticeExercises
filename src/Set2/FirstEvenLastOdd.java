10c178198431b1a622c4a4d15993ee0c72ca3979
Ladydiana <diana.culincu@gmail.com>
2018-02-25 17:25:19 +0200

    You read numbers until meeting 0. Find the first even and last odd number in the series you read.

---
+++ src/Set2/FirstEvenLastOdd.java
@@ -0,0 +1,12 @@
+/*
+ * You read numbers until meeting 0. Find the first even and last odd number \
+ * in the series you read.
+ */
+
+package Set2;
+
+import java.util.Scanner;
+
+public class FirstEvenLastOdd {
+    
+}
