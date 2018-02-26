9a6f7be3b4a24e6ba0a4950578445ede7fa5e9d7
Ladydiana <diana.culincu@gmail.com>
2018-02-26 17:54:05 +0200

    You read a natural number n and a digit k. Decide if the digit k appears inside n (different from the first and last digit).

---
+++ src/Set2/NumberContainsDigit.java
@@ -0,0 +1,15 @@
+/*
+ * You read a natural number n and a digit k. Decide if the digit k appears \
+ * inside n (different from the first and last digit).
+ */
+
+package Set2;
+
+import java.util.Scanner;
+
+public class NumberContainsDigit {
+    
+    public static void main(String args[]) {
+        
+    }
+}
