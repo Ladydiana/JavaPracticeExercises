50e63e761712e20a5543c3975be01cb4eddbfd51
Ladydiana <diana.culincu@gmail.com>
2018-02-28 18:01:49 +0200

    Find the medium weight and height for the n students from a classroom, with each weight and height read from keyboard.

---
+++ src/Set2/MediumHeightAndWeight.java
@@ -0,0 +1,29 @@
+/*
+ * Find the medium weight and height for the n students from a classroom, with each weight and height read from keyboard.
+ */
+
+package Set2;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MediumHeightAndWeight {
+    
+    public ArrayList<Double> weights;
+    public ArrayList<Double> heights;
+    public int n;
+    
+    public void readNumberOfStudents() {
+        System.out.print("n= ");
+        Scanner s = new Scanner(System.in);
+        this.n = s.nextInt();
+        checkNumberOfStudents();
+    } 
+    
+    public void checkNumberOfStudents() {
+        if(n<=0) {
+            System.err.println("Classroom has no students.");
+            System.exit(0);
+        } 
+    }
+}
