f36cb9f63bf80f81b3d74d68bf433848fec43790
Ladydiana <diana.culincu@gmail.com>
2018-02-28 18:36:12 +0200

    Find the medium weight and height for the n students from a classroom, with each weight and height read from keyboard.

--- src/Set2/MediumHeightAndWeight.java
+++ src/Set2/MediumHeightAndWeight.java
@@ -14,6 +14,11 @@ public class MediumHeightAndWeight {
     public ArrayList<Double> heights;
     public int n;
     
+    public MediumHeightAndWeight() {
+        weights = new ArrayList<>();
+        heights = new ArrayList<>();
+    }
+    
     public void readNumberOfStudents() {
         System.out.print("n= ");
         Scanner s = new Scanner(System.in);
@@ -25,6 +30,35 @@ public class MediumHeightAndWeight {
         if(n<=0) {
             System.err.println("Classroom has no students.");
             System.exit(0);
-        } 
+        }      
+    }
+    
+    public void readWeightAndHeight() {
+        Scanner s = new Scanner(System.in);
+        for(int i=0; i<n; i++) {
+            System.out.print("Height of student " + (i+1) + ": ");
+            heights.add(s.nextDouble());
+            System.out.print("\nWeight of student " + (i+1) + ": ");
+            weights.add(s.nextDouble());
+        }
+        s.close();
+    }
+    
+    public double computeMedianHeight() {
+        return heights.stream().mapToDouble(Double::doubleValue).sum()/n;
+    }
+    
+    public double computeMedianWeight() {
+        return weights.stream().mapToDouble(Double::doubleValue).sum()/n;
+    }
+    
+    public static void main(String args[]) {
+        MediumHeightAndWeight mhaw = new MediumHeightAndWeight();
+        mhaw.readNumberOfStudents();
+        mhaw.readWeightAndHeight();
+        System.out.println("Medium height for the classroom: " + 
+                            mhaw.computeMedianHeight());
+        System.out.println("Medium weight for the classroom: " + 
+                            mhaw.computeMedianWeight());
     }
 }
