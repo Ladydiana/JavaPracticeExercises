20db8e76fdd247b72684f3218bb7814c68717ef1
Ladydiana <diana.culincu@gmail.com>
2018-03-03 20:47:54 +0200

    Simplify a fraction a/b, where a and b are different from 0.

---
+++ src/Set2/SimplifyFraction.java
@@ -0,0 +1,12 @@
+/*
+ * Simplify a fraction a/b, where a and b are different from 0.
+ * Ex: 20/8 = 10/4 = 5/2  
+ */
+
+package Set2;
+
+import java.util.Scanner;
+
+public class SimplifyFraction {
+    
+}
